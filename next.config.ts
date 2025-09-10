96bb57895c70283b6036d8f1850f23e68fd92b68
Davi <N4linkWiki>
2025-09-10 16:53:54 -0300

    css

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,12 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
+// next.config.js
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  // Se for Vercel, ignora essa parte (eles já cuidam do build)
-  // Se for GitHub Pages, isso habilita exportação estática
+const nextConfig = {
   output: "export",
-
-  // Para imagens funcionarem no GitHub Pages
-  images: {
-    unoptimized: true,
-  },
-
-  // Isso resolve o problema do caminho quebrado no GitHub Pages
+  images: { unoptimized: true },
   basePath: isProd ? "/NIMBUS" : "",
   assetPrefix: isProd ? "/NIMBUS/" : "",
-
-  // Para não quebrar o build por causa do ESLint na Vercel
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
